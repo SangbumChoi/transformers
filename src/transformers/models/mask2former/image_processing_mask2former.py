d38c86dfd3b4d9d2af5a0a1480629f71ada79eff
sangbumchoi <danielsejong55@gmail.com>
2024-03-21 01:46:04 +0000

    tuple related update

--- src/transformers/models/mask2former/image_processing_mask2former.py
+++ src/transformers/models/mask2former/image_processing_mask2former.py
@@ -1123,9 +1123,10 @@ class Mask2FormerImageProcessor(BaseImageProcessor):
 
             segmentation = torch.zeros((384, 384)) - 1
             if target_sizes is not None:
-                segmentation = torch.zeros(target_sizes[i]) - 1
+                size = target_sizes[i] if isinstance(target_sizes[i], tuple) else target_sizes[i].cpu().tolist()
+                segmentation = torch.zeros(size) - 1
                 pred_masks = torch.nn.functional.interpolate(
-                    pred_masks.unsqueeze(0), size=target_sizes[i], mode="nearest"
+                    pred_masks.unsqueeze(0), size=size, mode="nearest"
                 )[0]
 
             instance_maps, segments = [], []
