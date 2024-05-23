de654a4bbf0498a3b95f8c1bce02da82dc54550b
sangbumchoi <danielsejong55@gmail.com>
2024-05-23 01:42:16 +0000

    make

--- src/transformers/models/yolov6/modeling_yolov6.py
+++ src/transformers/models/yolov6/modeling_yolov6.py
@@ -12,8 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" PyTorch YOLOV6 model."""
-
+"""PyTorch YOLOV6 model."""
 
 import math
 import warnings
