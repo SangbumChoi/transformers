6cef4019fd89a906f0d2d87553c27ce3db4fb141
sangbumchoi <danielsejong55@gmail.com>
2025-01-13 05:20:13 +0000

    fix the bug of v2 processor

--- src/transformers/models/detr/image_processing_detr.py
+++ src/transformers/models/detr/image_processing_detr.py
@@ -2041,7 +2041,7 @@ class DetrImageProcessor(BaseImageProcessor):
             pred_scores = scores_per_image * mask_scores_per_image
             pred_classes = labels_per_image
 
-            mask_pred, pred_scores, pred_classes = remove_low_and_no_objects(
+            pred_masks, pred_scores, pred_classes = remove_low_and_no_objects(
                 mask_pred, pred_scores, pred_classes, threshold, num_classes
             )
 
